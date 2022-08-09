05bfc2393519a2ff0790dd4326ddb75b7c7386ac
hilmihs <hilmisobandi@gmail.com>
2022-08-09 20:23:06 +0700

    initialize project

---
+++ src/components/ChatBox.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export default class ChatBox extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return (<div>
+            <h3>React Chat</h3>
+        </div> )
+    }
+}
---
+++ src/components/ChatOutput.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ChatOutput() {
+  return (
+    <div>ChatOutput</div>
+  )
+}
